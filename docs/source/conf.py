6f1afacd855e2bc10c8a8ee58bb47f4ebf67ff0e
yangfa <yangfa1027@gmail.com>
2025-05-24 16:27:19 +0800

    Add napoleon and viewcode extensions to Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
 ]
+
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
